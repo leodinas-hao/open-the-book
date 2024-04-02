d9da4855435be7698f1eb0335775843c350ca182
Leo Hao <lhao@macquarietelecom.com>
2024-04-02 15:38:59 +1100

    test: update epub test case

--- tests/test_epub.py
+++ tests/test_epub.py
@@ -2,20 +2,23 @@ from zipfile import ZipFile
 
 import pytest
 
-container_xml = '''<?xml version="1.0"?>
-<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
-  <rootfiles>
-    <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
-  </rootfiles>
-</container>
-'''
 
-
-@pytest.mark.parametrize('output', [
-  'temp/simpe_epub.epub'
+@pytest.mark.parametrize('output, book', [
+  ('temp/simple_epub.epub', {
+    'title': 'test',
+    'author': 'author1',
+    'uid': '1234567890',
+    'chapters': [
+      {'index': 0, 'name': 'c0', 'content': 'chapter 0 content'},
+      {'index': 1, 'name': 'c1', 'content': 'chapter 1 content'},
+    ]
+  }),
 ])
-def test_simple_epub(output):
+def test_simple_epub(output, book):
   # https://www.manuel-strehl.de/simple_epub_ebooks_with_python
-  with zipfile.ZipFile(output, 'w') as epub:
+  with ZipFile(output, 'w') as epub:
     # The first file must be named "mimetype"
-    epub.writestr("mimetype", "application/epub+zip")
+    # epub.writestr("mimetype", "application/epub+zip")
+    epub.write('src/open_the_book/templates/mimetype', arcname='mimetype')
+    epub.write('src/open_the_book/templates/container.xml', arcname='META-INF/container.xml')
+    epub.write('src/open_the_book/templates/style.css', arcname='OEBPS/style.css')
