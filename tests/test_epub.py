7c40a636b515954a3d06d01dcb75be2e98ab1d4d
Leo Hao <lhao@macquarietelecom.com>
2024-04-02 16:06:51 +1100

    refactor: update templates

--- tests/test_epub.py
+++ tests/test_epub.py
@@ -1,6 +1,17 @@
 from zipfile import ZipFile
 
 import pytest
+from jinja2 import Template
+
+
+def read(file: str) -> str:
+  with open(file, 'r') as f:
+    return f.read()
+
+
+def render_template(template_file: str, data):
+  t = Template(read(template_file))
+  return t.render(data)
 
 
 @pytest.mark.parametrize('output, book', [
@@ -22,3 +33,10 @@ def test_simple_epub(output, book):
     epub.write('src/open_the_book/templates/mimetype', arcname='mimetype')
     epub.write('src/open_the_book/templates/container.xml', arcname='META-INF/container.xml')
     epub.write('src/open_the_book/templates/style.css', arcname='OEBPS/style.css')
+
+    epub.writestr('OEBPS/book-toc.html', render_template('src/open_the_book/templates/book-toc.html.j2', book))
+    epub.writestr('OEBPS/toc.ncx', render_template('src/open_the_book/templates/toc.ncx.j2', book))
+    epub.writestr('OEBPS/content.opf', render_template('src/open_the_book/templates/content.opf.j2', book))
+    epub.writestr('OEBPS/cover.html', render_template('src/open_the_book/templates/cover.html.j2', book))
+    for ch in book['chapters']:
+      epub.writestr(f'OEBPS/{ch["index"]}.html', render_template('src/open_the_book/templates/chapter.html.j2', ch))
